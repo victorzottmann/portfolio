73cd8368b50f7663432752e06a9d162db40246e2
Victor Zottmann <zottmann.victor@gmail.com>
2024-02-29 17:06:29 +1100

    cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,48 @@
-import { programmingProjects, audioProjects } from "./projects.js";
+import { programmingProjects, audioProjects } from "./data/projects.js";
 import { showProgrammingProject, showAudioProject } from "./utils.js";
 
 const defaultCategory = "Programming Projects";
 
-document.addEventListener("DOMContentLoaded", () => {
-  function showProjects(category) {
-    const projectsContainer = document.querySelector(".projects-container");
-    projectsContainer.innerHTML = "";
+function showProjects(category) {
+  const projectsContainer = document.querySelector(".projects-container");
+  projectsContainer.innerHTML = "";
 
-    const projects =
-      category === defaultCategory ? programmingProjects : audioProjects;
+  const projects =
+    category === defaultCategory ? programmingProjects : audioProjects;
 
-    projects.forEach((project) => {
-      let card;
+  projects.forEach((project) => {
+    let card;
 
-      if (category === defaultCategory) {
-        card = showProgrammingProject(project);
-      } else {
-        card = showAudioProject(project);
-      }
-
-      projectsContainer.insertAdjacentHTML("beforeend", card);
-    });
-  }
-
-  function makeActive(clickedItem) {
-    const listItems = document.querySelectorAll(".projects-nav__item");
-    listItems.forEach((item) => item.classList.remove("active"));
-    clickedItem.classList.add("active");
-  }
-
-  function handleProjectCategoryClick(e) {
-    if (e.target.classList.contains("projects-nav__item")) {
-      const item = e.target;
-      const category = e.target.textContent;
-      makeActive(item);
-      showProjects(category);
+    if (category === defaultCategory) {
+      card = showProgrammingProject(project);
+    } else {
+      card = showAudioProject(project);
     }
-  }
 
-  function setDefaultProjects() {
-    showProjects(defaultCategory);
-  }
+    projectsContainer.insertAdjacentHTML("beforeend", card);
+  });
+}
 
+function makeActive(clickedItem) {
+  const listItems = document.querySelectorAll(".projects-nav__item");
+  listItems.forEach((item) => item.classList.remove("active"));
+  clickedItem.classList.add("active");
+}
+
+function handleProjectCategoryClick(e) {
+  if (e.target.classList.contains("projects-nav__item")) {
+    const item = e.target;
+    const category = e.target.textContent;
+    makeActive(item);
+    showProjects(category);
+  }
+}
+
+function setDefaultProjects() {
+  showProjects(defaultCategory);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
   const projectsCategory = document.querySelector(".projects-nav__list");
   projectsCategory.addEventListener("click", handleProjectCategoryClick);
 
